6401ef34669bf58e65e00be786b76dc9fd25b05f
Thett Oo Eain <thettoo99@gmail.com>
2019-07-19 13:31:00 +0800

    Adding superuser admin account

--- python_workshop/food_review/admin.py
+++ python_workshop/food_review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Restaurant, Restaurant_Review,Review_Detail
 # Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Restaurant_Review)
+admin.site.register(Review_Detail)
--- python_workshop/food_review/models.py
+++ python_workshop/food_review/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    
+    name = models.CharField(max_length=200 )
+    address = models.CharField(max_length=500)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Restaurant_Review(models.Model):
+    
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+    review = models.CharField(max_length=500)
+    rating = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    service_charges = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return '{}/{}/{}/{}'.format(self.review, self.rating, self.restaurant, self.price)
+
+class Review_Detail(models.Model):
+    
+    restaurant_review = models.ForeignKey('Restaurant_Review', on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return '{}/{}'.format(self.comment,self.restaurant_review)
