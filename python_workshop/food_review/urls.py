0fc2f5bb060aeb04a570d490d0861a508cdd80d6
Thett Oo Eain <thettoo99@gmail.com>
2019-07-19 15:24:19 +0800

    Create User Login Page

--- python_workshop/food_review/urls.py
+++ python_workshop/food_review/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('account/login/', LoginView.as_view(template_name='login.html'), name="login"),
     
 ]
