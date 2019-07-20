7054098fdc00f4331899d151e1d6fac959393cb7
Thett Oo Eain <thettoo99@gmail.com>
2019-07-21 00:03:19 +0800

    Adding Login Page

--- python_workshop/python_workshop/urls.py
+++ python_workshop/python_workshop/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',v.register, name='register'),
     path('',include('food_review.urls')),
+    path('',include('django.contrib.auth.urls')),
 ]
